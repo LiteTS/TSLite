274961d698c463f49144ff7146c7fa90566b90d7
Chawla <i56036@verisk.com>
2017-05-12 11:42:31 -0400

    Adding Tabs for Grid

--- TSLite/src/app/app.module.ts
+++ TSLite/src/app/app.module.ts
@@ -2,17 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { ImportsRootComponent } from './imports-root/imports-root.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImportsRootComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ TSLite/src/app/imports-root/imports-root.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-imports-root',
+  templateUrl: './imports-root.component.html',
+  styleUrls: ['./imports-root.component.css']
+})
+export class ImportsRootComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

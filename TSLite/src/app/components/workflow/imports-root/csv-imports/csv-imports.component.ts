885b2e70f35365370d2560c990b0b7e0bdaa204f
Sharma <i57568@verisk.com>
2017-05-16 13:53:13 -0400

    Fixed IgGrid Error

--- TSLite/src/app/components/workflow/imports-root/csv-imports/csv-imports.component.ts
+++ TSLite/src/app/components/workflow/imports-root/csv-imports/csv-imports.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CSVImportModel } from "../../../../models/imports-models/csvimport.model";
+
 
 declare var jQuery: any;
 @Component({
@@ -7,27 +9,28 @@ declare var jQuery: any;
   styleUrls: ['./csv-imports.component.css']
 })
 export class CsvImportsComponent implements OnInit {
- private gridOptions: IgGrid;
-   public id: string = "grid1";
+  private gridOptions: IgGrid;
+  public id: string = "grid1";
+  public csvFileImportList: CSVImportModel[] = [];
   constructor() { }
 
   ngOnInit() {
 
-     this.gridOptions = {
-            //dataSource: this.contracts,
-            width: "100%",
-            primaryKey: "contractSID",
-            responseDataKey: "results",
-            autoCommit: true,
-            autoGenerateColumns: false,
-            columns: [
-                { "headerText": "userLineOfBusiness", "key": "userLineOfBusiness", "dataType": "string", "width": "10%" },
-                { "headerText": "insuredName", "key": "insuredName", "dataType": "string", "width": "10%" },
-                { "headerText": "submitStatusCode", "key": "submitStatusCode", "dataType": "string", "width": "10%" },
-                { "headerText": "userDefined1", "key": "userDefined1", "dataType": "string", "width": "10%" },
-            ]
-            
-        };
+    this.gridOptions = {
+      //dataSource: this.contracts,
+      width: "100%",
+      primaryKey: "contractSID",
+      responseDataKey: "results",
+      autoCommit: true,
+      autoGenerateColumns: false,
+      columns: [
+        { "headerText": "userLineOfBusiness", "key": "userLineOfBusiness", "dataType": "string", "width": "10%" },
+        { "headerText": "insuredName", "key": "insuredName", "dataType": "string", "width": "10%" },
+        { "headerText": "submitStatusCode", "key": "submitStatusCode", "dataType": "string", "width": "10%" },
+        { "headerText": "userDefined1", "key": "userDefined1", "dataType": "string", "width": "10%" },
+      ]
+
+    };
   }
 
 }

a743d2a8507dc8ed961053b4606145fe82304bf2
Chawla <i56036@verisk.com>
2017-05-16 11:47:59 -0400

    updating app Module

--- TSLite/src/app/app.module.ts
+++ TSLite/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { ImportsRootComponent } from './components/imports-root/imports-root.component';
+import { ImportsRootComponent } from './components/workflow/imports-root/imports-root.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
 import { DashboardComponent } from './components/dashboard/dashboard.component';

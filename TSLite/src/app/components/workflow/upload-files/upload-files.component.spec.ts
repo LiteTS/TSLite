6356485788077eec0e5e054b7c9a1212eff578d5
Chawla <i56036@verisk.com>
2017-05-17 14:42:01 -0400

    alingnment

--- TSLite/src/app/components/workflow/upload-files/upload-files.component.spec.ts
+++ TSLite/src/app/components/workflow/upload-files/upload-files.component.spec.ts
@@ -22,4 +22,7 @@ describe('UploadFilesComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+
+  
 });
--- TSLite/src/app/components/workflow/upload-files/upload-files.component.ts
+++ TSLite/src/app/components/workflow/upload-files/upload-files.component.ts
@@ -15,16 +15,37 @@ export class UploadFilesComponent implements OnInit {
   }
 
 
- public uploader:FileUploader = new FileUploader({url: URL});
-  public hasBaseDropZoneOver:boolean = false;
-  public hasAnotherDropZoneOver:boolean = false;
+  public uploader: FileUploader = new FileUploader({ url: URL });
+  public hasBaseDropZoneOver: boolean = false;
+  public hasAnotherDropZoneOver: boolean = false;
 
-  public fileOverBase(e:any):void {
+  public fileOverBase(e: any): void {
     this.hasBaseDropZoneOver = e;
   }
 
-  public fileOverAnother(e:any):void {
+  public fileOverAnother(e: any): void {
     this.hasAnotherDropZoneOver = e;
+  }
+
+  public searchImport() {
+    // Declare variables 
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("inputTable");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("importTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
   }
 
 }

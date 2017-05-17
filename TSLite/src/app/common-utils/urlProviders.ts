f669a2a655f33d98ed140d104befdf368e8f4d04
Chawla <i56036@verisk.com>
2017-05-17 16:10:48 -0400

    adding few service components

---
+++ TSLite/src/app/common-utils/urlProviders.ts
@@ -0,0 +1,7 @@
+export class URLConst {
+    static readonly domain: string = "http://localhost:41858";
+}
+
+export class MessageBusTypes {
+
+}
--- TSLite/src/app/components/workflow/imports-root/csv-imports/csv-imports.component.ts
+++ TSLite/src/app/components/workflow/imports-root/csv-imports/csv-imports.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CSVImportModel } from "../../../../models/imports-models/csvimport.model";
-
-
 declare var jQuery: any;
 @Component({
   selector: 'tslite-csv-imports',
@@ -10,25 +8,26 @@ declare var jQuery: any;
 })
 export class CsvImportsComponent implements OnInit {
   private csvgridOptions: IgGrid;
-  public id: string = "csvid";
+  private id: string = "csvGrid";
+  private listOfContractFiles: any[] = ["abc", "xyz", "lac"];
+  private listOfLocationFiles: any[] = ["abc", "xyz", "roc"];
+  private listOfDatabaseNames: any[] = ["a", "v", "c"];
+  private listOfExposureSetNames: any[] = ["a", "c", "d"];
   public csvFileImportList: CSVImportModel[] = [];
   constructor() {
     this.csvFileImportList = [
       { importId: 1, fileNameContract: 'abc', fileNameLocation: 'efg', databaseName: 'AIRExposure', exposureSetName: 'abc' }
     ];
   }
-
   ngOnInit() {
-
-
     this.csvgridOptions = {
       //dataSource: this.contracts,
       width: "100%",
       primaryKey: "importId",
-      responseDataKey: "results",
       autoCommit: true,
       autoGenerateColumns: false,
       columns: [
+        { "headerText": "importId", "key": "importId", "dataType": "number", "width": "10%", hidden: true },
         { "headerText": "Contract", "key": "fileNameContract", "dataType": "string", "width": "10%" },
         { "headerText": "Location", "key": "fileNameLocation", "dataType": "string", "width": "10%" },
         { "headerText": "Database", "key": "databaseName", "dataType": "string", "width": "10%" },
@@ -37,6 +36,62 @@ export class CsvImportsComponent implements OnInit {
       features: [
         {
           name: "Updating",
+          columnSettings: [{
+            //The combo is defined as an editor provider. Combo options are defined under 'editorOptions'.
+            columnKey: "fileNameContract",
+            editorType: 'combo',
+            required: true,
+            editorOptions: {
+              mode: 'dropdown',
+              readOnly: false,
+              dataSource: this.listOfContractFiles,
+              textKey: "value",
+              valueKey: "value",
+              enableClearButton: true,
+            }
+          },
+          {
+            //The combo is defined as an editor provider. Combo options are defined under 'editorOptions'.
+            columnKey: "fileNameLocation",
+            editorType: 'combo',
+            required: true,
+            editorOptions: {
+              readOnly: false,
+              mode: 'dropdown',
+              dataSource: this.listOfLocationFiles,
+              textKey: "value",
+              valueKey: "value",
+              enableClearButton: true,
+            }
+          },
+          {
+            //The combo is defined as an editor provider. Combo options are defined under 'editorOptions'.
+            columnKey: "databaseName",
+            editorType: 'combo',
+            required: true,
+            editorOptions: {
+              readOnly: false,
+              dataSource: this.listOfDatabaseNames,
+              textKey: "value",
+              valueKey: "value",
+              enableClearButton: true,
+              allowCustomValue: true,
+            }
+          }, {
+            //The combo is defined as an editor provider. Combo options are defined under 'editorOptions'.
+            columnKey: "exposureSetName",
+            editorType: 'combo',
+            required: true,
+            editorOptions: {
+              readOnly: false,
+              dataSource: this.listOfExposureSetNames,
+              textKey: "value",
+              valueKey: "value",
+              enableClearButton: true,
+              allowCustomValue: true,
+            }
+          }
+          ]
         },
         {
           name: "Selection",
@@ -49,8 +104,6 @@ export class CsvImportsComponent implements OnInit {
           name: "Sorting"
         }
       ]
-
     };
   }
-
 }
